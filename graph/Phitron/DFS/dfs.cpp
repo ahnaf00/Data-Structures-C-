8bf3e99d4d5d525bbce0840f7362cb09e6ea9684
Ahnaf Anan <aanana0171@gmail.com>
2025-02-09 17:17:26 +0600

    DFS

---
+++ graph/Phitron/DFS/dfs.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj_list;
+vector<int> vis;
+
+void dfs(int src)
+{
+    cout << src << " ";
+    vis[src] = true;
+
+    for(int x : adj_list[src])
+    {
+        if(!vis[x])
+        {
+            dfs(x);
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    adj_list.resize(n+1);
+    vis.assign(n+1, false);
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    dfs(0);
+    
+
+    return 0;
+}
