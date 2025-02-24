6d3b6b4fb98decbe8fcee3da060156585a827505
Ahnaf Anan <aanana0171@gmail.com>
2025-02-25 00:47:03 +0600

    Detect negative weighted cycle with Floyd-Warshall Algorithm

---
+++ graph/Phitron/Floyd-Warshall/detect_negative_weight_with_floyd_warshall.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj_mat;
+
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    adj_mat.resize(n,vector<int>(n,INT_MAX));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i==j)
+            {
+                adj_mat[i][j] = 0;
+            }
+        }
+        
+    }
+    
+
+    for (int i = 0; i < e; i++)
+    {
+        int a,b,c;
+        cin >> a >> b >> c;
+        
+        adj_mat[a][b] = c;
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if(adj_mat[i][k] != INT_MAX && adj_mat[k][j] != INT_MAX && adj_mat[i][k]+adj_mat[k][j] < adj_mat[i][j])
+                {
+                    adj_mat[i][j] = adj_mat[i][k]+adj_mat[k][j]; 
+                }
+            }
+        }
+    }
+
+    bool cycle = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(adj_mat[i][i] < 0)
+        {
+            cycle = true;
+        }
+    }
+    
+    if(cycle)
+    {
+        cout << "Negative weighted cycle detected" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if(adj_mat[i][j] == INT_MAX)
+                {
+                    cout << "INF ";
+                }
+                else
+                {
+                    cout << adj_mat[i][j] << " ";
+                }
+            }
+            cout << endl;
+        }
+    }    
+
+    return 0;
+}
